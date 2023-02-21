0692cade272cc5df64d5ecb501787c8f85b59393
Egor104 <egor.grig104@gmail.com>
2023-02-21 12:43:32 +0500

    Перенес 'cars' в 'CarService', внедрил зависимость через конструктор

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,29 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController extends CarServiceImpl {
 
-    CarServiceImpl carService = new CarServiceImpl();
-    static List<Car> cars = new ArrayList<>();
+    private final CarServiceImpl carServiceImpl;
 
-    static {
-        cars.add(new Car("Tesla", "white", 300));
-        cars.add(new Car("Ferrari", "red", 350));
-        cars.add(new Car("Porsche", "yellow", 240));
-        cars.add(new Car("Lexus", "black", 220));
-        cars.add(new Car("Honda", "green", 200));
+    public CarController(CarServiceImpl carServiceImpl) {
+        this.carServiceImpl = carServiceImpl;
     }
 
     @GetMapping(value = "/cars")
     public String printCar(@RequestParam(value = "count", defaultValue = "5") Integer count, Model model) {
-        model.addAttribute("cars", carService.getSomeCars(cars, count));
+        model.addAttribute("cars", carServiceImpl.getSomeCars(cars, count));
         return "car";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,9 +1,22 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService {
+
+    public static List<Car> cars = new ArrayList<>();
+
+    static {
+        cars.add(new Car("Tesla", "white", 300));
+        cars.add(new Car("Ferrari", "red", 350));
+        cars.add(new Car("Porsche", "yellow", 240));
+        cars.add(new Car("Lexus", "black", 220));
+        cars.add(new Car("Honda", "green", 200));
+    }
 
     @Override
     public List<Car> getSomeCars(List<Car> cars, Integer count) {
