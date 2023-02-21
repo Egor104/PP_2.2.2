62d067d3c657241a8b5560f8343e8981b50429c3
Egor104 <egor.grig104@gmail.com>
2023-02-21 12:57:03 +0500

    BugFix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,20 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
+import static web.service.CarServiceImpl.cars;
 
 @Controller
-public class CarController extends CarServiceImpl {
+public class CarController {
 
-    private final CarServiceImpl carServiceImpl;
+    private final CarService carService;
 
-    public CarController(CarServiceImpl carServiceImpl) {
-        this.carServiceImpl = carServiceImpl;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String printCar(@RequestParam(value = "count", defaultValue = "5") Integer count, Model model) {
-        model.addAttribute("cars", carServiceImpl.getSomeCars(cars, count));
+        model.addAttribute("cars", carService.getSomeCars(cars, count));
         return "car";
     }
 }
